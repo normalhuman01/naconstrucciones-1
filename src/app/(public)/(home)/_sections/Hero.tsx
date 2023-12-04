86e24d83e82e90e8e6ac96f423f19063d8667f90
Darwin Vilcachagua Ayala <darwin97.va@gmail.com>
2023-12-04 18:31:17 -0500

    hero

--- src/app/(public)/(home)/_sections/Hero.tsx
+++ src/app/(public)/(home)/_sections/Hero.tsx
@@ -1,5 +1,24 @@
 export const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="text-center my-8 max-w-[760px] mx-auto">
+      <span>SOMOS EXPERTOS EN SERVICIOS DE</span>
+      <h3 className="mb-6 mt-2 text-3xl font-bold">
+        INGENIERÍA, CONSTRUCCIÓN Y <br /> MANTENIMIENTO
+      </h3>
+
+      <p>
+        En <strong>N&A INGENIERÍA Y CONSTRUCCIONES</strong> contamos con amplia
+        experiencia en desarrollar y gestionar proyectos de diseño, arquitectura
+        y construcción.
+      </p>
+      <br />
+      <p>
+        La conformamos un <strong>equipo de profesionales</strong> y{" "}
+        <strong>especialistas</strong> en diversas áreas que comprometidos con
+        cada <strong>proyecto</strong>, buscamos ofrecer permanentemente ideas y soluciones
+        <strong>innovadoras</strong> para satisfacer plenamente las necesidades de nuestros
+        clientes.
+      </p>
+    </div>
+  );
+};
