894e24de8ab93cc2d576fd5effe53e0ba680e32f
Darwin Vilcachagua Ayala <darwin97.va@gmail.com>
2023-12-05 23:10:56 -0500

    carousel brands responsive

--- src/components/custom/BrandsCarousel/index.tsx
+++ src/components/custom/BrandsCarousel/index.tsx
@@ -6,20 +6,20 @@ import "react-multi-carousel/lib/styles.css";
 const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 9,
+    breakpoint: { max: 4000, min: 1600 },
+    items: 8,
   },
   desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 9,
+    breakpoint: { max: 1600, min: 1024 },
+    items: 6,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 9,
+    items: 4,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
-    items: 9,
+    items: 2,
   },
 };
 
