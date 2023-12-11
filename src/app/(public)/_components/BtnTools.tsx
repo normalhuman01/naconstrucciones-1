f1661161d0f39662b7a58130a73cd5b390f66602
Darwin Vilcachagua Ayala <darwin97.va@gmail.com>
2023-12-11 17:14:12 -0500

    tools menu

--- src/app/(public)/_components/BtnTools.tsx
+++ src/app/(public)/_components/BtnTools.tsx
@@ -3,6 +3,7 @@
 import { cn } from "@/lib/utils";
 import { TProductCategory } from "@/types";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Fragment, HTMLAttributes, useEffect, useState } from "react";
 import { IoClose } from "react-icons/io5";
 
@@ -30,6 +31,17 @@ const linksSecondary = [
   "Otros",
 ];
 
+type TLinkData = {
+  label: string;
+} & (
+  | {
+      link: string;
+    }
+  | {
+      links: TLinkData[];
+    }
+);
+
 export const BtnTools = ({
   categories,
 }: {
@@ -37,6 +49,7 @@ export const BtnTools = ({
 }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [menu, setMenu] = useState(0);
+  const pathname = usePathname();
 
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
@@ -52,7 +65,7 @@ export const BtnTools = ({
     };
   }, []);
 
-  const data = [
+  const data: TLinkData[] = [
     {
       label: "INICIO",
       link: "/",
@@ -62,10 +75,12 @@ export const BtnTools = ({
       links: [
         {
           label: "EPP",
-          links: categories.map((category) => ({
-            label: category.name,
-            link: `/productos/epp/${category.slug}#products`,
-          })),
+          links: categories
+            .map((category) => ({
+              label: category.name,
+              link: `/productos/epp/${category.slug}#products`,
+            }))
+            .reverse(),
         },
         {
           label: "FERRETERÍA",
@@ -84,13 +99,19 @@ export const BtnTools = ({
           label: "ASESORÍAS",
           link: "/servicios/asesorias",
         },
+        {
+          label: "PORTAFOLIO",
+          link: "/servicios/portafolio",
+        },
       ],
     },
     {
       label: "CONTACTOS",
+      link: "/contacto",
     },
   ];
 
+  const currentMenu = data[menu];
   return (
     <>
       <div
@@ -110,49 +131,101 @@ export const BtnTools = ({
       {isOpen && (
         <div className="top-0 left-0 z-[10000] fixed p-3 flex justify-center items-center w-full h-[100vh] bg-dark text-white">
           <div className="max-w-[1120px] mx-auto relative flex justify-center items-center w-full">
-            <div className="flex gap-9 items-stretch overflow-hidden max-h-[90vh] w-full">
-              <div className="flex flex-col gap-2 items-start">
-                {data.map((item, index) => {
-                  return (
-                    <Fragment key={index}>
-                      {index ? <hr className="w-full bg-white" /> : null}
-                      <BtnPrimary onClick={() => setMenu(index)} key={index}>
-                        {item.label}
-                      </BtnPrimary>
-                    </Fragment>
-                  );
-                })}
+            <div className="relative flex gap-6 items-stretch max-h-[90vh] w-full">
+              <div className="flex flex-col gap-2 justify-between">
+                <div className="flex flex-col gap-2 items-start">
+                  {data.map((item, index) => {
+                    return (
+                      <Fragment key={index}>
+                        {index ? <hr className="w-full bg-white" /> : null}
+                        <BtnPrimary
+                          onClick={() => setMenu(index)}
+                          key={index}
+                          className="text-md md:text-xl"
+                        >
+                          {item.label}
+                        </BtnPrimary>
+                      </Fragment>
+                    );
+                  })}
+                </div>
+                <div>
+                  <img
+                    src="/img/logo/large-logo-dark.png"
+                    width={220}
+                    alt=""
+                    className="max-w-[160px] md:max-w-[220px]"
+                  />
+                </div>
               </div>
               <div className="h-[100vh] w-[1px] bg-white"></div>
               <div className="flex flex-col">
-                <div className="text-xl">{data[menu]?.label}</div>
-                <ul className="mb-4">
-                  {data[menu]?.links?.map((item, index) => {
-                    return (
-                      <li
-                        key={index}
-                        className={cn(
-                          "ml-5 text-xl",
-                          index ? "text-[rgba(255,255,255,0.5)]" : ""
-                        )}
-                      >
-                        <Link
-                          href={item.link || ""}
-                          onClick={() => setIsOpen(false)}
+                {"links" in currentMenu && (
+                  <ul className="mb-4">
+                    {currentMenu?.links?.map((item, index) => {
+                      const hasLink = "link" in item;
+                      return (
+                        <li
+                          key={index}
+                          className={cn(
+                            "ml-3 text-md md:text-xl mb-5",
+                            index ? "text-[rgba(255,255,255,0.5)]" : ""
+                          )}
                         >
-                          {item.label}
-                        </Link>
-                      </li>
-                    );
-                  })}
-                </ul>
+                          {hasLink && (
+                            <Link
+                              href={item.link || ""}
+                              onClick={() => setIsOpen(false)}
+                            >
+                              {item.label}
+                            </Link>
+                          )}
+                          {!hasLink && (
+                            <div>
+                              <div>{item.label}</div>
+                              <ul>
+                                {item.links?.map((link, index) => (
+                                  <li
+                                    key={index}
+                                    className="ml-3 text-md md:text-xl mb-2 text-[rgba(255,255,255,0.5)]"
+                                  >
+                                    {"link" in link && (
+                                      <Link
+                                        href={link.link || ""}
+                                        onClick={() => setIsOpen(false)}
+                                        className={cn(
+                                          pathname ===
+                                            link.link.split("#")[0] &&
+                                            "text-white"
+                                        )}
+                                      >
+                                        {link.label}
+                                      </Link>
+                                    )}
+                                  </li>
+                                ))}
+                              </ul>
+                            </div>
+                          )}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                )}
               </div>
+              <img
+                src="/img/icons/redes.png"
+                width={180}
+                height={60}
+                alt=""
+                className="absolute right-3 md:right-5 bottom-0 cursor-pointer"
+              />
+              <IoClose
+                onClick={() => setIsOpen(false)}
+                className="text-[3rem] text-white absolute top-[-2rem] md:top-5 right-3 md:right-5 cursor-pointer"
+              />
             </div>
           </div>
-          <IoClose
-            onClick={() => setIsOpen(false)}
-            className="text-[3rem] text-white absolute top-3 md:top-5 right-3 md:right-5 cursor-pointer"
-          />
         </div>
       )}
     </>
